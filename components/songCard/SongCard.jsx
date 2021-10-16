2248face328c5e7c645b24d62a5251ec8d8e8492
Jackson <jacksonmain9@gmail.com>
2021-10-16 16:26:58 -0500

    added noreferrer and noopener to song tag

--- components/songCard/SongCard.jsx
+++ components/songCard/SongCard.jsx
@@ -2,7 +2,7 @@ import s from "./SongCard.module.css";
 
 export default function SongCard({ song }) {
   return (
-    <a className={s.card} target="_blank" href={song.url}>
+    <a className={s.card} rel="noreferrer noopener" target="_blank" href={song.url}>
       <div className={s.card__container}>
         <img className={s.card__img} src={song.img} alt={song.alt} />
         <p className={s.card__author}>{song.author}</p>
