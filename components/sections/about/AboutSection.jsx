afb58cd76a7f1c2e6de9f7e5d02138bf8e94f636
Jackson <jacksonmain9@gmail.com>
2021-10-18 12:07:47 -0500

    fixed about section grid for chrome

--- components/sections/about/AboutSection.jsx
+++ components/sections/about/AboutSection.jsx
@@ -17,6 +17,7 @@ export default function AboutSection() {
               lyricism delivered over hard-hitting instrumentals.
             </p>
           </div>
+
           <div role="img" className={s.about__img} alt="faaris sitting down" />
         </div>
       </div>
