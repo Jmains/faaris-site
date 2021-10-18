72cd41fbae3b36d40b467d10648a424d8f82da72
Jackson <jacksonmain9@gmail.com>
2021-10-18 13:33:56 -0500

    added link for Presidential in about section

--- components/sections/about/AboutSection.jsx
+++ components/sections/about/AboutSection.jsx
@@ -8,9 +8,17 @@ export default function AboutSection() {
           <div className={s.about__textWrapper}>
             <h2 className={s.about__title}>About</h2>
             <p className={s.about__description}>
-              Faaris Sattar, better known by his stage name Faaris, is a 23 y.o. singer and
-              rapper, born and raised in Austin, TX. He broke into music scene with his first
-              song Presidential, released in month 2021.
+              Faaris Sattar, better known by his stage name Faaris, is a 23 y.o.
+              <span style={{ color: "var(--pri-btnColor)" }}> rapper</span> and
+              <span style={{ color: "var(--pri-btnColor)" }}> lyricist</span>, born and raised
+              in Austin, TX. He broke into music scene with his first song{" "}
+              <a
+                style={{ textDecoration: "underline" }}
+                href="https://open.spotify.com/album/1lWSYvfg0ur3I5eNmghk8G"
+              >
+                Presidential
+              </a>
+              , released in month 2021.
             </p>
             <p style={{ marginTop: "1rem" }} className={s.about__description}>
               Since then, he has been building his career as a performer with his clever
